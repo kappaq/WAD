06ff8bdef42a8f248757ddbc8f782c02db551354
kappaq <32762329+kappaq@users.noreply.github.com>
2018-05-07 01:04:49 +0300

    Add project files.

---
+++ test/Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test.Controllers
+{   [Authorize]
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+        public ActionResult Update()
+        {
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            return View();
+        }
+        public ActionResult History()
+        {
+            return View();
+        }
+    }
+}
---
+++ test/Controllers/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProductDetails()
+        {
+            return View();
+        }
+        public ActionResult AddToCart()
+        {
+            return View();
+        }
+        public ActionResult OrderDetails()
+        {
+            return View();
+        }
+        public ActionResult PlaceOrder()
+        {
+            return View();
+        }
+
+
+    }
+}
